ccf5cf3ddb7bafbf92639651e8b8661a114c5f96
alpharii <alphari404@gmail.com>
2024-05-17 13:57:25 +0700

    add button components  and create structure folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/Elements/Button/Button'
 
 function App() {
-  return (
-    <div>  
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return(
+    <div>
+      <Button/>
     </div>
-  )  
+  )
 }
 
 export default App
---
+++ src/components/Elements/Button/Button.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Button = () => {
+  return (
+    <div>
+        <button className='px-3 py-2 rounded-md bg-blue-400 text-slate-950 font-medium'>
+            Click Me
+        </button>
+    </div>
+  )
+}
+
+export default Button
